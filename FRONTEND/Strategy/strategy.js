2135d0bac5b3383f1a35f93e14242369926a3186
Martin-Boyanov <mboyanov09@gmail.com>
2024-05-20 15:07:51 +0300

    Completed the onclick functions

--- FRONTEND/Strategy/strategy.js
+++ FRONTEND/Strategy/strategy.js
@@ -1,3 +1,12 @@
-function goToAscent{
-    window.location.href= '/Maps/Ascent'
+function goToAscent(){
+    window.location.href = '/Maps/Ascent'
+}
+function goToBind(){
+    window.location.href = '/Maps/Bind'
+}
+function goToBreeze(){
+    window.location.href = '/Maps/Breeze'
+}
+function goToIcebox(){
+    window.location.href = '/Maps/Icebox'
 }
