ed2c6e00e2d11b91dc14487387587d48f70a2221
Martin-Boyanov <mboyanov09@gmail.com>
2024-05-20 20:37:07 +0300

    styled the strategy page more.

--- FRONTEND/Strategy/strategy.js
+++ FRONTEND/Strategy/strategy.js
@@ -10,3 +10,19 @@ function goToBreeze(){
 function goToIcebox(){
     window.location.href = '/Maps/Icebox'
 }
+
+//Drop down menu
+document.addEventListener('DOMContentLoaded', function() {
+    const menuButton = document.getElementById('menuButton');
+    const popupMenu = document.getElementById('popupMenu');
+
+    menuButton.addEventListener('click', (event) => {
+        popupMenu.style.display = popupMenu.style.display === 'block' ? 'none' : 'block';
+    });
+
+    document.addEventListener('click', function(event) {
+        if (!popupMenu.contains(event.target) && event.target !== menuButton) {
+            popupMenu.style.display = 'none';
+        }
+    });
+});
